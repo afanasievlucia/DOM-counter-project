b8a1997812f9e055efcc8357b0803beeff68cf7d
Afanasiev <afanasiev.lucia@gmail.com>
2021-04-23 16:18:49 +0300

    another way to write

--- dom.js
+++ dom.js
@@ -1,3 +1,4 @@
+/*
 let button1 = document.getElementById("plus").addEventListener("click", increment);
 let button2 = document.getElementById("minus").addEventListener("click", decrement);
 
@@ -14,5 +15,28 @@ function decrement() {
     } else {
     x--;
     document.getElementById("minus").value = x;    
+    }
+}*/
+
+//  another way to write
+let button1 = document.getElementById("plus");
+let button2 = document.getElementById("minus");
+
+button1.addEventListener("click", increment);
+button2.addEventListener("click", decrement);
+
+let i = 0;
+function increment() {
+    i++;
+    button1.value = i;
+};
+
+let x = 100;
+function decrement() {
+    if (x <= 0) {
+    button2.value = 'Stop';    
+    } else {
+    x--;
+    button2.value = x;    
     }
 }
